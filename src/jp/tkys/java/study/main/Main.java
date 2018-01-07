9e0bad64dbacf63c42a4899c12c4975cb8eb4adc
Takayoshi <tkys0515@gmail.com>
2018-01-08 07:17:24 +0900

    Added note for java program execution (FQCN).

--- src/jp/tkys/java/study/main/Main.java
+++ src/jp/tkys/java/study/main/Main.java
@@ -3,7 +3,7 @@ package jp.tkys.java.study.main;
 public class Main {
 
     /**
-     * Javaプログラムを実行するには、mainメソッドが含まれているクラスの正式名を指定する必要がある
+     * Javaプログラムを実行するには、mainメソッドが含まれているクラスの完全限定クラス名(Full Qualified Class Name; FQCN)を指定する必要がある
      * $java jp.tkys.java.study.main.Main
      * 注：ディレクトリのツリー構造を名前空間に合わせておく
      * 注：カレントディレクトリを先頭ディレクトリの直上("jp"の直上)に設定しておく
