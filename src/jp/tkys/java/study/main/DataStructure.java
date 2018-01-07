3c40f6b7b2344fb6157c13437209600a7d7717d5
Takayoshi <tkys0515@gmail.com>
2018-01-08 06:01:08 +0900

    Added note for array initialization.

--- src/jp/tkys/java/study/main/DataStructure.java
+++ src/jp/tkys/java/study/main/DataStructure.java
@@ -6,5 +6,10 @@ public class DataStructure {
         int[] arrayVar1 = new int[5];
         int[] arrayVar2 = new int[] {0, 1, 2, 3, 4};
         int[] arrayVar3 = {0, 1, 2, 3, 4};
+
+        // Java VMの動作
+        // ・int型の要素を5個分のメモリ領域を確保
+        // ・int型の配列変数arrayVar1のメモリ領域を確保
+        // ・arrayVar1に配列先頭要素のアドレスを入れる
     }
 }
