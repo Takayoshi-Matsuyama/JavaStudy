0fd138abb4e586a61a9c0577dd1f9b69925d7125
Takayoshi <tkys0515@gmail.com>
2018-01-08 06:39:21 +0900

    Added note for method signature.

--- src/jp/tkys/java/study/main/Method.java
+++ src/jp/tkys/java/study/main/Method.java
@@ -9,4 +9,9 @@ public class Method {
     private static void testMethod(String parameter) {
         // 仮引数 (parameter)
     }
+
+    private static void testMethod(String parameterA, String parameterB) {
+        // シグネチャ：メソッド名、仮引数の個数・型・並び順
+        // (戻り値の型はシグネチャに含まれない)
+    }
 }
