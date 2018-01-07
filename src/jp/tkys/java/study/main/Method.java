720c3f3f1646107c15437ed4c43241637078e7e1
Takayoshi <tkys0515@gmail.com>
2018-01-08 06:32:07 +0900

    Added method call.

---
+++ src/jp/tkys/java/study/main/Method.java
@@ -0,0 +1,12 @@
+package jp.tkys.java.study.main;
+
+public class Method {
+
+    private static void methodCall() {
+        testMethod("実引数 (argument)");
+    }
+
+    private static void testMethod(String parameter) {
+        // 仮引数 (parameter)
+    }
+}
