0b35e72a5eb442d74d26c22b0b8e38190df90d6a
Takayoshi <tkys0515@gmail.com>
2018-01-07 17:26:15 +0900

    Added basic data types.

--- src/jp/tkys/java/study/main/Main.java
+++ src/jp/tkys/java/study/main/Main.java
@@ -5,4 +5,25 @@ public class Main {
     public static void main(String[] args) {
 	    System.out.println("Hello World");
     }
+
+    private static void dataType() {
+        // 整数
+        long longVar;
+        int intVar;
+        short shortVar;
+        byte byteVar;
+
+        // 小数
+        double doubleVar;
+        float floatVar;
+
+        // 真偽値
+        boolean boolVar;
+
+        // 文字
+        char charVar;
+
+        // 文字列
+        String stringVar;
+    }
 }
